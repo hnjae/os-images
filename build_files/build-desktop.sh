84b28a540f42a38a3da5e452b2a23dde3113d270
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-03-24 21:44:39 +0900

    feat(desktop): remove 1Password from base image packages

--- build_files/build-desktop.sh
+++ build_files/build-desktop.sh
@@ -7,22 +7,6 @@ source /ctx/common.sh
 
 ### Desktop-specific packages and settings
 
-# 1Password
-# Redirect /opt to /usr/lib/opt so files land in the immutable image layer
-# (default /opt -> /var/opt is stateful and gets wiped on first boot)
-install -d /usr/lib/opt
-ln -sfn usr/lib/opt /opt
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-cat >/etc/yum.repos.d/1password.repo <<'REPO'
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://downloads.1password.com/linux/keys/1password.asc
-REPO
-
 curl -fsSLo /etc/yum.repos.d/brave-browser.repo \
     https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
@@ -39,11 +23,7 @@ dnf5 install \
     --setopt=install_weak_deps=True \
     --enablerepo=terra,terra-extras,terra-mesa,rpmfusion-free,rpmfusion-nonfree \
     --assumeyes \
-    1password 1password-cli brave-browser firefox nushell
-
-# Declare 1password system group via sysusers.d (bootc lint compliance)
-echo 'g onepassword - -' >/usr/lib/sysusers.d/onepassword.conf
-echo 'g onepassword-cli - -' >>/usr/lib/sysusers.d/onepassword.conf
+    brave-browser firefox nushell
 
 ### Remove packages
 dnf5 autoremove --assumeyes
