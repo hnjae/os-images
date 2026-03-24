3ad7214b2e3252d7c102bf94383c29bb582c7420
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-03-24 21:59:43 +0900

    feat: remove browser packages and switch nushell to nu
    - Remove Brave and Firefox browser installations from build-desktop.sh
    - Update common.sh to replace nushell package installation with nu
    - Remove custom nushell repository configuration
    - Keep other system packages and settings unchanged

--- build_files/build-desktop.sh
+++ build_files/build-desktop.sh
@@ -7,14 +7,5 @@ source /ctx/common.sh
 
 ### Desktop-specific packages and settings
 
-curl -fsSLo /etc/yum.repos.d/brave-browser.repo \
-    https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
-dnf5 install \
-    --setopt=install_weak_deps=True \
-    --enablerepo=terra,terra-extras,terra-mesa,rpmfusion-free,rpmfusion-nonfree \
-    --assumeyes \
-    brave-browser firefox
-
 ### Remove packages
 dnf5 autoremove --assumeyes
